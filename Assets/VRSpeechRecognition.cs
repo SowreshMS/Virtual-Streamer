965bc18570d9d58a2db74659d7d05ae1e4490af9
Sowresh Mecheri-Senthil <sowreshms@gmail.com>
2023-11-23 03:12:35 -0600

    with code

--- Assets/VRSpeechRecognition.cs
+++ Assets/VRSpeechRecognition.cs
@@ -1,17 +1,32 @@
-using System;
 using UnityEngine;
 using System.Diagnostics;
-
+using System.Text;
+using System.Collections;
 
 public class VRSpeechRecognition : MonoBehaviour
 {
+    private Process pythonProcess;
+
     void Start()
     {
-        // Path to your Python script
-        string pythonScriptPath = "C:\\Users\\Spher\\Downloads\\sockets.py";
+        UnityEngine.Debug.Log("hello");
+        StartCoroutine(ActivatePythonScriptAfterDelay());
+    }
 
-        // Create a process to run the Python script
-        Process process = new Process
+    IEnumerator ActivatePythonScriptAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
+
+        StartPythonScript();
+    }
+
+    void StartPythonScript()
+    {
+        UnityEngine.Debug.Log("Python time");
+
+        string pythonScriptPath = @"C:/Users/Spher/Downloads/sockets.py";
+
+        pythonProcess = new Process
         {
             StartInfo = new ProcessStartInfo
             {
@@ -19,16 +34,42 @@ public class VRSpeechRecognition : MonoBehaviour
                 Arguments = $"{pythonScriptPath}",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
+                RedirectStandardError = true,
                 CreateNoWindow = true
-            }
+            },
+            EnableRaisingEvents = true // Allow capturing process exit event
         };
 
-        // Start the process and capture its output
-        process.Start();
-        string output = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
+        // Event handler for capturing the process exit event
+        pythonProcess.Exited += (sender, e) =>
+        {
+            string output = pythonProcess.StandardOutput.ReadToEnd();
+            string errorOutput = pythonProcess.StandardError.ReadToEnd();
 
-        // Do something with the output (e.g., update Unity UI)
-        UnityEngine.Debug.Log(output);
+            if (!string.IsNullOrEmpty(output))
+            {
+                UnityEngine.Debug.Log("Python script output: " + output);
+            }
+
+            if (!string.IsNullOrEmpty(errorOutput))
+            {
+                UnityEngine.Debug.LogError("Python script error: " + errorOutput);
+            }
+
+            pythonProcess.Dispose(); // Dispose of the process to free resources
+        };
+
+        UnityEngine.Debug.Log("Python time part2");
+        pythonProcess.Start();
+    }
+
+    // Optionally, you may want to stop the Python process when the Unity application quits
+    private void OnApplicationQuit()
+    {
+        if (pythonProcess != null && !pythonProcess.HasExited)
+        {
+            pythonProcess.Kill();
+            pythonProcess.Dispose();
+        }
     }
 }
