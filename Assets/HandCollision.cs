23e11bb04ff954c3b4808098940114e34f2a3618
FireboltLegend <aik210000@utdallas.edu>
2023-12-09 14:43:56 -0600

    a

--- Assets/HandCollision.cs
+++ Assets/HandCollision.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class HandCollision : MonoBehaviour
 {
     [SerializeField] private GameObject rig;
+    GameObject avatar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        avatar = GameObject.FindGameObjectWithTag("Avatar");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check for collision with the avatar
+        if (Vector3.Distance(transform.position, avatar.transform.position) < 2.0f)
+        {
+            rig.gameObject.SetActive(true);
+        }
+        else
+        {
+            rig.gameObject.SetActive(false);
+        }
     }
-
     private void OnTriggerEnter(Collider other)
     {
         // Check if the colliding GameObject has the tag "Avatar"
@@ -24,7 +32,7 @@ public class HandCollision : MonoBehaviour
         {
             // Perform actions when the collision with an "Avatar" occurs
             // Debug.Log("Collision with Avatar detected!");
-            rig.gameObject.SetActive(true);
+            // rig.gameObject.SetActive(true);
             // You can add more actions or functions here as needed
         }
     }
@@ -36,7 +44,7 @@ public class HandCollision : MonoBehaviour
         {
             // Perform actions when the collision with an "Avatar" ends
             // Debug.Log("Collision with Avatar exited!");
-            rig.gameObject.SetActive(false);
+            // rig.gameObject.SetActive(false);
             // You can add more actions or functions here as needed
         }
     }
