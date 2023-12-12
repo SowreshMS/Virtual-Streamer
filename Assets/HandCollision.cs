ce4d6ff6056075014d7ed4d0a963f0f3dd229774
Jose Garcia <132632885+Horachepe@users.noreply.github.com>
2023-12-12 15:38:30 -0600

    Bhaptics

--- Assets/HandCollision.cs
+++ Assets/HandCollision.cs
@@ -11,40 +11,77 @@ public class HandCollision : MonoBehaviour
     
     GameObject avatar;
     GameObject hands;
+    GameObject spine;
+    GameObject headset;
+    GameObject RHFollow;
+    GameObject LHFollow;
+    GameObject RHLocation;
+    GameObject LHLocation;
+    Vector3 prevPosRH;
+    Vector3 prevPosLH;
     Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         avatar = GameObject.FindGameObjectWithTag("Avatar");
         hands = GameObject.FindGameObjectWithTag("Avatar Hands");
+        spine = GameObject.FindGameObjectWithTag("Avatar Spine");
+        headset = GameObject.FindGameObjectWithTag("MainCamera");
+        RHFollow = GameObject.FindGameObjectWithTag("RH Follow");
+        LHFollow = GameObject.FindGameObjectWithTag("LH Follow");
+        RHLocation = GameObject.FindGameObjectWithTag("RH Location");
+        LHLocation = GameObject.FindGameObjectWithTag("LH Location");
         animator = avatar.GetComponent<Animator>();
+        prevPosRH = RHFollow.transform.position;
+        prevPosLH = LHFollow.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check for collision with the avatar
-        if (Vector3.Distance(transform.position, hands.transform.position) < 1.5f)
+        // Check if the distance between the headset and the spine is less than 0.8f
+        if (Vector3.Distance(headset.transform.position, spine.transform.position) < 0.8f)
         {
-            // rig.gameObject.SetActive(true);
-            // avatar.GetComponent<RigBuilder>().enabled = true;
-            animator.SetBool("Shaking", true);
-            TestHaptic();
-        }
-        else
-        {
-            // rig.gameObject.SetActive(false);
-            // avatar.GetComponent<RigBuilder>().enabled = false;
-            animator.SetBool("Shaking", false);
-        }
-        if (Vector3.Distance(transform.position, avatar.transform.position) < 1.0f)
-        {
+            // Set the rig to true and enable the RigBuilder
             rig.gameObject.SetActive(true);
             avatar.GetComponent<RigBuilder>().enabled = true;
+            // Play the haptic feedback
+            BhapticsLibrary.Play("vest");
+            BhapticsLibrary.Play("left_hand");
+            BhapticsLibrary.Play("right_hand");
+            // Set the shaking animation to false
+            animator.SetBool("Shaking", false);
+            RHFollow.transform.position = Vector3.MoveTowards(RHFollow.transform.position, RHLocation.transform.position, 0.01f);
+            LHFollow.transform.position = Vector3.MoveTowards(LHFollow.transform.position, LHLocation.transform.position, 0.01f);
         }
+        // Check if the distance between the user hand and the avatar hand is less than 1.25f
+        else if (Vector3.Distance(transform.position, hands.transform.position) < 1.25f)
+        {
+            RHFollow.transform.position = Vector3.MoveTowards(RHFollow.transform.position, prevPosRH, 0.01f);
+            LHFollow.transform.position = Vector3.MoveTowards(LHFollow.transform.position, prevPosLH, 0.01f);
+            // Stop the rig
+            rig.gameObject.SetActive(false);
+            // Disable the RigBuilder
+            avatar.GetComponent<RigBuilder>().enabled = false;
+            // Set the shaking animation to true
+            animator.SetBool("Shaking", true);
+            // Check if the distance between the user hand and the avatar hand is less than 0.25f
+            if (Vector3.Distance(transform.position, hands.transform.position) < 0.25f)
+            {
+                // Play the haptic feedback
+                BhapticsLibrary.Play("right_hand");
+            }
+        }
+        // Set the rig to false and disable the RigBuilder
         else
         {
+            RHFollow.transform.position = Vector3.MoveTowards(RHFollow.transform.position, prevPosRH, 0.01f);
+            LHFollow.transform.position = Vector3.MoveTowards(LHFollow.transform.position, prevPosLH, 0.01f);
+            // Set the shaking animation to false
+            animator.SetBool("Shaking", false);
+            // Stop the rig
             rig.gameObject.SetActive(false);
+            // Disable the RigBuilder
             avatar.GetComponent<RigBuilder>().enabled = false;
         }
     }
--- Assets/HandFollow.cs
+++ Assets/HandFollow.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class HandFollow : MonoBehaviour
 {
-    [SerializeField] private GameObject userRightHand;
-    [SerializeField] private GameObject userLeftHand;
-    [SerializeField] private GameObject avatarFollowRightHand;
-    [SerializeField] private GameObject avatarFollowLeftHand;
-    private Vector3 prevPosRH;
-    private Vector3 prevPosLH;
-    private Vector3 currPosRH;
-    private Vector3 currPosLH;
-    private Vector3 changeRH;
-    private Vector3 changeLH;
+    // [SerializeField] private GameObject userRightHand;
+    // [SerializeField] private GameObject userLeftHand;
+    // [SerializeField] private GameObject avatarFollowRightHand;
+    // [SerializeField] private GameObject avatarFollowLeftHand;
+    // private Vector3 prevPosRH;
+    // private Vector3 prevPosLH;
+    // private Vector3 currPosRH;
+    // private Vector3 currPosLH;
+    // private Vector3 changeRH;
+    // private Vector3 changeLH;
     // Start is called before the first frame update
     void Start()
     {
-        prevPosRH = userRightHand.transform.position;
-        prevPosLH = userLeftHand.transform.position;
-        currPosRH = userRightHand.transform.position;
-        currPosLH = userLeftHand.transform.position;
+        // prevPosRH = userRightHand.transform.position;
+        // prevPosLH = userLeftHand.transform.position;
+        // currPosRH = userRightHand.transform.position;
+        // currPosLH = userLeftHand.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currPosRH = userRightHand.transform.position;
-        currPosLH = userLeftHand.transform.position;
-        changeRH = currPosRH - prevPosRH;
-        changeLH = currPosLH - prevPosLH;
-        changeRH.x *= -1f;
-        changeLH.x *= -1f;
-        changeRH.z *= -1f;
-        changeLH.z *= -1f;
-        avatarFollowRightHand.transform.position += 0.75f * changeRH;
-        avatarFollowLeftHand.transform.position += 0.75f * changeLH;
-        prevPosRH = currPosRH;
-        prevPosLH = currPosLH;
+        // currPosRH = userRightHand.transform.position;
+        // currPosLH = userLeftHand.transform.position;
+        // changeRH = currPosRH - prevPosRH;
+        // changeLH = currPosLH - prevPosLH;
+        // changeRH.x *= -1f;
+        // changeLH.x *= -1f;
+        // changeRH.z *= -1f;
+        // changeLH.z *= -1f;
+        // avatarFollowRightHand.transform.position += 0.75f * changeRH;
+        // avatarFollowLeftHand.transform.position += 0.75f * changeLH;
+        // prevPosRH = currPosRH;
+        // prevPosLH = currPosLH;
     }
 }
